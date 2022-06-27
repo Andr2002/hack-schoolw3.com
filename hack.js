dc5bd52107f5cd77fb668dc5d1cc24f72f6bdfce
ANDR <54762489+Andr2002@users.noreply.github.com>
2022-06-28 02:11:05 +0500

    Update hack.js

--- hack.js
+++ hack.js
@@ -2,18 +2,56 @@ import { hack } from './hackClass.js';
 
 const hacks = new hack();
 
-await hacks.JS()
-    .then((res) => {
-        console.log(`${res.time}\n${res.points}`);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+//  puppeteer уебан косожопый какой-то 
+//  каждый раз разные ошибки выкидывает
+//  если за ним понаблюдать, то можно увидеть как он сам по себе живет
+//  это ебать не библиотека, какой-то искуственный интеллект
+//  только тупой пиздец...
 
-await hacks.HTML()
-    .then((res) => {
-        console.log('Время в секундах: ' + res);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+//  поэтому въебал цикл
+while (true) {
+    await executeCSS();
+}
+
+// await executeHTML();
+
+// await executeJS();
+
+async function executeHTML() {
+    console.log('Начинается выполнение теста по HTML...');
+
+    await hacks.HTML()
+        .then((res) => {
+            console.log('Время в секундах: ' + res);
+            console.log('\nStatus: заебись\n');
+        })
+        .catch((err) => {
+            console.log('\nStatus: хуйня полная\n');
+            // console.error(err);
+        });
+}
+
+async function executeCSS() {
+    console.log('Начинается выполнение теста по CSS...');
+
+    await hacks.CSS()
+        .then((res) => {
+            console.log(`${res.time}\n${res.points}`);
+        })
+        .catch((err) => {
+            // console.log(err);
+            console.log('Все хуйня, давай по новой');
+        });
+}
+
+async function executeJS() {
+    console.log('Начинается выполнение теста по JavaScript...');
+
+    await hacks.JS()
+        .then((res) => {
+            console.log(`${res.time}\n${res.points}`);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
